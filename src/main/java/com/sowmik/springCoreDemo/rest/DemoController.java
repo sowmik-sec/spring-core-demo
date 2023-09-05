92eb0d68c1dbf8b9f632e15d740a4750ca228125
sowmik-sec <sowmik.sec@gmail.com>
2023-09-05 10:24:58 +0600

    qualifiers coding

--- src/main/java/com/sowmik/springCoreDemo/rest/DemoController.java
+++ src/main/java/com/sowmik/springCoreDemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.sowmik.springCoreDemo.rest;
 
 import com.sowmik.springCoreDemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
     @GetMapping("/dailyworkout")
